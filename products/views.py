f38edf53fcb207873536af2c5646613acc925a7d
Aryankaushik541 <Aryankaushik541@gmail.com>
2025-12-01 16:45:41 +0530

    Add product views

---
+++ products/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category
+from .serializers import ProductSerializer, ProductListSerializer, CategorySerializer
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'featured']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at', 'name']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        return ProductSerializer
+
+    @action(detail=False, methods=['get'])
+    def featured(self, request):
+        featured_products = self.queryset.filter(featured=True)
+        serializer = self.get_serializer(featured_products, many=True)
+        return Response(serializer.data)
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
